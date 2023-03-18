5db3badee782435d4073302e3cf57fad67a16836
Emiliano Carreño <emiliano0662@gmail.com>
2023-03-17 23:49:58 -0500

    actualizacion webpack instagram

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, './public'),
